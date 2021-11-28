3aa03d88d099ddbc73c22e73d6855eb8c19e1d42
vnasa <vnasa@LAPTOP-VETU9ACB>
2021-11-28 20:35:25 -0300

    one commit

--- Controller/FatorialController.java
+++ Controller/FatorialController.java
@@ -3,7 +3,35 @@ package Controller;
 public class FatorialController {
 
 	public FatorialController() {
-		// TODO Auto-generated constructor stub
+	 super();
+	 
 	}
 
+	// public int fatorial (int numero) essa linha vai receber um numero inteiro
+	
+	public int fat (int fatorial, int multiplicador) {
+	
+		
+		//condição de parada
+	
+		// se o numero for igual a um retorna um
+	
+	   // if (numero <= 1)
+	   // return 1;
+		
+		
+		if (multiplicador == 1) {
+		
+		return 1;
+	
+	   } else {
+		   // int proximo = numero - 1; // o proximo numero do fatorial é o numero - 1  // ou numero - 1 * fatorial
+		   // return numero * fatorial (proximo);
+		   
+		   multiplicador = fatorial-multiplicador;
+		   return 1- fat (multiplicador,fatorial);
+	   }
+	
+	}
+		
 }
--- View/Principal.java
+++ View/Principal.java
@@ -1,10 +1,24 @@
 package View;
 
+import Controller.FatorialController;
+
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		 FatorialController fatC = new FatorialController();
 
+		 //
+		 
+		 int multiplicador = 4;
+		 int fatorial = 5 ; 
+		 int fat = fatC.fat(fatorial,multiplicador);
+		 
+		 System.out.println(fat);
+		 
+		 
 	}
 
+
 }
+
